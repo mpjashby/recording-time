1eb9ba05ba34bb9117edd40a534217a25b57292a
Matt Ashby <matthew.ashby@ntu.ac.uk>
2020-06-15 18:10:13 +0100

    Initial manuscript submission

---
+++ initialise.R
@@ -0,0 +1,10 @@
+# LOAD PACKAGES AND FUNCTIONS
+
+# Load all packages and custom functions in this file, which is then loaded at
+# the start of every other file
+
+
+
+# load packages
+library("lubridate")
+library("tidyverse")
